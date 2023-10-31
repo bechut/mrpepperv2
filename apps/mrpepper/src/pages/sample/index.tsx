acf5e0cf9b66ef7a5471527dccce1be19ec023d8
you@example.com <you@example.com>
2023-10-31 14:53:02 +0700

    add pages folder, lib types

---
+++ apps/mrpepper/src/pages/sample/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import { ISampleProps } from '@mrpepper/types';
+
+const Page: FC<ISampleProps> = () => {
+  return <div>Sample Page</div>;
+};
+
+export default Page;
