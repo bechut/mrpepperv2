93ec69023dfc70f9c2a18bc59ee9055c1ae2d3c5
you@example.com <you@example.com>
2023-10-31 16:52:35 +0700

    config: antd, routing, pages destiantion

---
+++ apps/mrpepper/src/assets/routes/index.ts
@@ -0,0 +1,7 @@
+import { IRoutes } from '@mrpepper/types';
+import sample from '../../pages/sample/route';
+
+export const routes: IRoutes[] = [
+    sample
+];
+export const locales = ['en', 'vi'];
