6ce1d0d513fcda7eed92ec39f0e252e931ff44dc
you@example.com <you@example.com>
2023-11-03 15:14:26 +0700

    Config redux toolkit, auto catch api message

--- apps/mrpepper/src/pages/sample/page.tsx
+++ apps/mrpepper/src/pages/sample/page.tsx
@@ -1,19 +1,37 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Button, Layout, Space, Typography } from 'antd';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 
 import { ISampleProps } from '@mrpepper/types';
+import { pepperActions, pepperRootState } from '@mrpepper/redux';
+import NonAuthLayout from '../../components/layout/non-auth';
 
 const { Text } = Typography;
 
 const Page: FC<ISampleProps> = (props) => {
+  const appStates = useSelector(
+    (states: pepperRootState) => states.appSlice,
+    shallowEqual
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(appStates)
+    dispatch(
+      pepperActions.appSlice.setAlert({ message: 'sample', status: true })
+    );
+  }, []);
+
   return (
-    <Layout style={{ height: '100vh' }}>
-      <Space direction="vertical">
-        <Text>Sample Page</Text>
-        <Text>Locale: {props.locale} </Text>
-        <Button type="primary">Testing</Button>
-      </Space>
-    </Layout>
+    <NonAuthLayout>
+      <Layout style={{ height: '100vh' }}>
+        <Space direction="vertical">
+          <Text>Sample Page</Text>
+          <Text>Locale: {props.locale} </Text>
+          <Button type="primary">Testing</Button>
+        </Space>
+      </Layout>
+    </NonAuthLayout>
   );
 };
 
