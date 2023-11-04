11b37c89c8c92070e7c6fd2a7a904ca7a5cd11be
you@example.com <you@example.com>
2023-11-04 17:24:54 +0700

    integrate sign up api (pending global languages)

--- apps/mrpepper/src/pages/signup/lang/en.ts
+++ apps/mrpepper/src/pages/signup/lang/en.ts
@@ -3,7 +3,7 @@ export default {
   'sign-up:error-msg?email_empty': 'Please input your email',
   'sign-up:error-msg?email_invalid_format': 'Invalid email format',
   'sign-up:form-label?username': 'Username',
-  'sign-up:error-msg?username': 'Please input your username',
+  'sign-up:error-msg?username_empty': 'Please input your username',
   'sign-up:form-label?password': 'Password',
   'sign-up:error-msg?password_empty': 'Please input your password',
   'sign-up:error-msg?password_length_8_characters':
