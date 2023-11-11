e32c49e91164b84993b5c86a558b9236683545cc
you@example.com <you@example.com>
2023-11-11 10:15:20 +0700

    push un commit code

--- apps/mrpepper/src/main.tsx
+++ apps/mrpepper/src/main.tsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import App from './app/app';
+import { BrowserRouter } from 'react-router-dom';
+import { Routers } from '@mrpepper/router';
+import { locales, routes } from './assets/routes';
+
+console.log(routes)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading</div>}>
+      <BrowserRouter>
+        <Routers locales={locales} routes={routes} />
+      </BrowserRouter>
+    </Suspense>
   </StrictMode>
 );
